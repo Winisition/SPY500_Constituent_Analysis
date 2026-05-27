22463dfa11b202384c11614d777661e54e5841fb
Winisition <winstonspam@outlook.com>
2026-05-27 23:47:34 +0800

    fix: set cwd to PROJECT_ROOT for dbt run

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from ingest import scrape_01_wikipedia, scrape_02_yfinance_tickers, scrape_03_yfinance_spy, check_01
 import subprocess
 from pathlib import Path
-from project_config import DB_PATH
+from project_config import DB_PATH, PROJECT_ROOT
 
 def run_dbt():
-    subprocess.run(["dbt", "run"], check=True)
-    subprocess.run(["dbt", "test"], check=True)
+    subprocess.run(["dbt", "run"], check=True, cwd=PROJECT_ROOT)
+    subprocess.run(["dbt", "test"], check=True, cwd=PROJECT_ROOT)
 
 def main():
     
